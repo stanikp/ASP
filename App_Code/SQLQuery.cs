6c781b676152a1c69814b9a9c345e3917915811f
Support UniBIT <support@unibit.bg>
2017-03-13 14:29:22 +0200

    13.03

---
+++ App_Code/SQLQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SQLQuery
+/// </summary>
+public class SQLQuery
+{
+    public const string NewsInsert = "Insert into news (Title, Author, Content, CreateDate, Image) VALUES(@Title, @Author, @Content, @CreateDate, @Image)";
+    public const string InsertActivationCode = "Insert INTO UserActivation Values(@UserId, @ActivationCode)";
+}
